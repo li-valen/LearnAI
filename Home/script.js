c7ee1e2885c1d635d46bab4f9d647c3ef1c4d245
Rishab Das <das.rishabk@gmail.com>
2023-06-17 20:46:46 -0400

    commit

--- Home/script.js
+++ Home/script.js
@@ -1,3 +1,13 @@
 let intro = document.querySelector('.intro');
 let logo = document.querySelector('.logo');
-let logoSpan = document.querySelectorAll('.PhillyMusicIsAmazing')
+let logoSpan = document.querySelectorAll('.PhillyMusicIsAmazing');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    setTimeout(()=>{
+        logoSpan.forEach((span, idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active');
+            }, (idx+1) * 400 )
+        })
+    })
+})
