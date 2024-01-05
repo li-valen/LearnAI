ac48eef81445a4b97da98f37f804678a3cd70b26
Rishab Das <das.rishabk@gmail.com>
2024-01-04 21:19:06 -0500

    remade index.html

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -1,8 +1,4 @@
-document.querySelector('.js-home-button').addEventListener('click', () => {
 
-    document.body.innerHTML = `<h1 id="new-h1-element>Hello World</h1>`;
-    document.getElementById("new-h1-element").style.textAlign = "center";
-});
 
 
 
