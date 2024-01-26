cc75acf8fc1c6e3fac81792d141ffb981188670c
Rishab Das <das.rishabk@gmail.com>
2024-01-25 19:55:39 -0500

    added scroll animation

--- Scripts/discover.js
+++ Scripts/discover.js
@@ -1,18 +1,20 @@
-const container = document.querySelector('.container');
-    let currentSection = 0;
-
-    window.addEventListener('wheel', (event) => {
-      if (event.deltaY > 0) {
-        // Scrolling down
-        currentSection++;
-      } else {
-        // Scrolling up
-        currentSection--;
-      }
-
-      // Ensure the currentSection stays within bounds
-      currentSection = Math.max(0, Math.min(container.children.length - 1, currentSection));
-
-      // Scroll horizontally to the current section
-      container.style.transform = `translateX(-${currentSection * 100}vw)`;
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show')
+        }
     });
+});
+
+
+
+
+
+
+
+const hiddenElements = document.querySelectorAll(".hidden");
+
+hiddenElements.forEach((el) => observer.observe(el));
