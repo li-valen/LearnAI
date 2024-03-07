857b2eb64c5025b13e9cebb9cb7c991cbf6ddd3f
Valen Li <117173876+li-valen@users.noreply.github.com>
2024-03-06 22:15:11 -0500

    added a bunch of stuff

---
+++ Scripts/interact.js
@@ -0,0 +1,11 @@
+var imageDetectionButton = document.querySelector("#imageDetectionButton");
+
+// imageDetectionButton.addEventListener("click", imageDetectionButton());
+
+
+function imageDetectionButton_() {
+    document.getElementById("title").innerHTML = "Hello World";
+    console.log("hello")
+}
+
+
