2b0f03b5073095bb1afff5ee7d8acbbb2504d1dc
Valen Li <117173876+li-valen@users.noreply.github.com>
2024-03-07 10:42:30 -0500

    added image

--- Scripts/general.js
+++ Scripts/general.js
@@ -11,3 +11,12 @@ $(function () {
       $("aside").fadeToggle("1000");
     });
   });
+
+  $("body").mousemove(function (a) {
+    var mouseX = (a.pageX * -1) / 20;
+    var mouseY = (a.pageY * -1) / 25;
+    $("#background-image").css(
+      "background-position",
+      mouseX + "px " + mouseY + "px"
+    );
+  });
