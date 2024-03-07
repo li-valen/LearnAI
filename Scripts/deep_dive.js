42a0633b9736fd5c78262488cc9af781676e5c4a
Valen Li <117173876+li-valen@users.noreply.github.com>
2024-03-06 20:54:39 -0500

    added files

--- Scripts/deep_dive.js
+++ Scripts/deep_dive.js
@@ -1,3 +1,17 @@
+$(function () {
+  $("#btnMenu").click(function () {
+    $("#btnMenu").toggleClass("change");
+    $("#btnMenu").addClass("colorWhite");
+    $(".secMainNavi li").each(function (i) {
+      var $li = $(this);
+      setTimeout(function () {
+        $li.toggleClass("naviSlideIn");
+      }, i * 50); // delay 100 ms
+    });
+    $("aside").fadeToggle("1000");
+  });
+});
+
 //preloader
 $(window).on("load", function() {
   let tl = gsap.timeline({ease: "slow"});
