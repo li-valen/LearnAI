5d2edb6a3531685646bfea8363c673c8783f6a38
Rishab Das <das.rishabk@gmail.com>
2024-03-23 11:39:38 -0400

    commit

--- Scripts/interact.js
+++ Scripts/interact.js
@@ -1,3 +1,79 @@
+
+async function loadModel() {
+  const model = await cocoSsd.load();
+  return model;
+}
+
+async function detectObjects(video, model) {
+  let vid = document.getElementById("vid");
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  let but = document.getElementById("but");
+  let closeButton_ = document.getElementById("close");
+  let video = document.getElementById("vid");
+  let mediaDevices = navigator.mediaDevices;
+  vid.muted = true;
+  but.addEventListener("click", () => {
+      // Accessing the user camera and video.
+      mediaDevices
+          .getUserMedia({
+              video: true,
+              audio: true,
+          })
+          .then((stream) => {
+              // Changing the source of video to current stream.
+              video.srcObject = stream;
+              video.addEventListener("loadedmetadata", () => {
+                  video.play();
+              });
+          })
+          .catch(alert);
+  });
+
+  closeButton_.addEventListener("click", () => {
+      video.srcObject.getTracks().forEach(function(track) {
+        track.stop();
+      });
+      video.srcObject = null;
+  });
+});
+
+
+
+
+
 $('#objectDetectionButton').on('click', function () {
     $('.center').fadeIn(500);
     $(this).fadeOut(500);
@@ -31,63 +107,3 @@ $('#diffusionClose').on('click', function () {
 
 
 
-// let slideIndex = 1;
-// showSlides(slideIndex);
-
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  let but = document.getElementById("but");
-  let closeButton_ = document.getElementById("close-cam");
-  let video = document.getElementById("vid");
-  let mediaDevices = navigator.mediaDevices;
-  vid.muted = true;
-  but.addEventListener("click", () => {
-      // Accessing the user camera and video.
-      mediaDevices
-          .getUserMedia({
-              video: true,
-              audio: true,
-          })
-          .then((stream) => {
-              // Changing the source of video to current stream.
-              video.srcObject = stream;
-              video.addEventListener("loadedmetadata", () => {
-                  video.play();
-              });
-          })
-          .catch(alert);
-  });
-
-  closeButton_.addEventListener("click", () => {
-      video.srcObject.getTracks().forEach(function(track) {
-        track.stop();
-      });
-      video.srcObject = null;
-  });
-});
