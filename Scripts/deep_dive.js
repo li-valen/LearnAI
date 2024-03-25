b4ed2fb4622264a2b03f27296b0d1a10e6aba517
Valen Li <117173876+li-valen@users.noreply.github.com>
2024-03-25 13:22:10 -0400

    menu animation finished

--- Scripts/deep_dive.js
+++ Scripts/deep_dive.js
@@ -1,5 +1,7 @@
 gsap.registerPlugin(ScrollTrigger);
 let count = 0;
+var box1 = document.getElementById("box1");
+var box2 = document.getElementById("box2");
 gsap.utils.toArray(".comparisonSection").forEach(section => {
 	let tl = gsap.timeline({
 			scrollTrigger: {
@@ -20,17 +22,17 @@ gsap.utils.toArray(".comparisonSection").forEach(section => {
 });
 
 function menuSlide() {
-
 	if(count == 0){
-		gsap.to("#box1", { duration: 1, x: "-105%" }); // Slide out box 1 to right
-		gsap.to("#box2", { duration: 1, x: "-40%", delay: 0.5 }); // Slide out box 2 to right with a delay
+		gsap.to("#box1", { duration: 1, x: "-160%", duration: 2, opacity: 1}); // Slide out box 1 to right
+		gsap.to("#box2", { duration: 1, x: "-105%", duration: 2, opacity: 1}); // Slide out box 2 to right with a delay
 		count = 1;
 	} else if(count == 1){
-		gsap.to("#box1", { duration: 1, x: "0%" }); // Slide in box 1 from right
-		gsap.to("#box2", { duration: 1, x: "0%", delay: 0.5 }); // Slide in box 2 from right with a delay
+		gsap.to("#box1", {x: "-260%", duration: 2, delay: 0.5}); // Slide in box 1 from right
+		gsap.to("#box2", {x: "-360%", duration: 3}); // Slide in box 2 from right with a delay
 		count = 0;
+		gsap.to("#box1", {x: "0%", duration: 0, delay: 3}); // Slide in box 1 from right
+		gsap.to("#box2", {x: "0%", duration: 0, delay: 3}); // Slide in box 2 from right with a delay
 	}
-
 }
 
 document.getElementById("animation").addEventListener("click", menuSlide);
