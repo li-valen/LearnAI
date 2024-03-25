19104ca3402c35eb9c13a0764ad0a626cbc1ac88
Valen Li <117173876+li-valen@users.noreply.github.com>
2024-03-25 16:04:26 -0400

    menu bar actually finished with a formula

--- Scripts/deep_dive.js
+++ Scripts/deep_dive.js
@@ -1,7 +1,10 @@
 gsap.registerPlugin(ScrollTrigger);
 let count = 0;
-var box1 = document.getElementById("box1");
-var box2 = document.getElementById("box2");
+let element = document.getElementById("menuSect");
+let positionInfo = element.getBoundingClientRect();
+let width = positionInfo.width * -1;
+element.style.right = width + "px";
+
 gsap.utils.toArray(".comparisonSection").forEach(section => {
 	let tl = gsap.timeline({
 			scrollTrigger: {
@@ -22,16 +25,20 @@ gsap.utils.toArray(".comparisonSection").forEach(section => {
 });
 
 function menuSlide() {
+	element = document.getElementById("menuSect");
+	positionInfo = element.getBoundingClientRect();
+	width = positionInfo.width * -1;
+	element.style.right = width + "px";
 	if(count == 0){
-		gsap.to("#box1", { duration: 1, x: "-160%", duration: 2, opacity: 1}); // Slide out box 1 to right
-		gsap.to("#box2", { duration: 1, x: "-105%", duration: 2, opacity: 1}); // Slide out box 2 to right with a delay
+		gsap.to(".menu", {xPercent: -160, duration: 2, opacity: 1}); // Slide out box 1 to right
+		gsap.to(".chatBox", {xPercent: -100, duration: 2, opacity: 1}); // Slide out box 2 to right with a delay
 		count = 1;
 	} else if(count == 1){
-		gsap.to("#box1", {x: "-260%", duration: 2, delay: 0.5}); // Slide in box 1 from right
-		gsap.to("#box2", {x: "-360%", duration: 3}); // Slide in box 2 from right with a delay
+		gsap.to(".menu", {xPercent: -260, duration: 2, delay: 0.5}); // Slide in box 1 from right
+		gsap.to(".chatBox", {xPercent: -360, duration: 3}); // Slide in box 2 from right with a delay
 		count = 0;
-		gsap.to("#box1", {x: "0%", duration: 0, delay: 3}); // Slide in box 1 from right
-		gsap.to("#box2", {x: "0%", duration: 0, delay: 3}); // Slide in box 2 from right with a delay
+		gsap.to(".menu", {xPercent: 0, duration: 0, delay: 3}); // Slide in box 1 from right
+		gsap.to(".chatBox", {xPercent: 0, duration: 0, delay: 3}); // Slide in box 2 from right with a delay
 	}
 }
 
