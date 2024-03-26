ac27c04954a356dc95b6df4e44647598a4fc4262
Valen Li <117173876+li-valen@users.noreply.github.com>
2024-03-25 21:01:10 -0400

    fade in

---
+++ Scripts/menu.js
@@ -0,0 +1,27 @@
+let count = 0;
+let element = document.getElementById("menuSect");
+let positionInfo = element.getBoundingClientRect();
+let width = positionInfo.width * -1;
+element.style.right = width + "px";
+
+function menuSlide() {
+	element = document.getElementById("menuSect");
+	positionInfo = element.getBoundingClientRect();
+	width = positionInfo.width * -1;
+	element.style.right = width + "px";
+	if(count == 0){
+		gsap.to(".menu", {xPercent: -160, duration: 2, opacity: 1}); // Slide out box 1 to right
+		gsap.to(".chatBox", {xPercent: -100, duration: 2, opacity: 1}); // Slide out box 2 to right with a delay
+    gsap.to("li", {opacity: 1, duration: 1, ease: "power2.in"});
+		count = 1;
+	} else if(count == 1){
+		gsap.to(".menu", {xPercent: -260, duration: 2, delay: 0.5}); // Slide in box 1 from right
+		gsap.to(".chatBox", {xPercent: -360, duration: 3}); // Slide in box 2 from right with a delay
+    gsap.to("li", {opacity: 0, duration: 1, ease: "power2.inOut"});
+		count = 0;
+		gsap.to(".menu", {xPercent: 0, duration: 0, delay: 3}); // Slide in box 1 from right
+		gsap.to(".chatBox", {xPercent: 0, duration: 0, delay: 3}); // Slide in box 2 from right with a delay
+	}
+}
+
+document.getElementById("animation").addEventListener("click", menuSlide);
