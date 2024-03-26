33419949d3fdf0107c39a4e5dc1c9989914744e0
Valen Li <117173876+li-valen@users.noreply.github.com>
2024-03-25 20:58:25 -0400

    added menu text and fade out animation

--- Scripts/deep_dive.js
+++ Scripts/deep_dive.js
@@ -1,9 +1,4 @@
 gsap.registerPlugin(ScrollTrigger);
-let count = 0;
-let element = document.getElementById("menuSect");
-let positionInfo = element.getBoundingClientRect();
-let width = positionInfo.width * -1;
-element.style.right = width + "px";
 
 gsap.utils.toArray(".comparisonSection").forEach(section => {
 	let tl = gsap.timeline({
@@ -23,23 +18,3 @@ gsap.utils.toArray(".comparisonSection").forEach(section => {
 	  // ...and the image the opposite way (at the same time)
 	  .fromTo(section.querySelector(".afterImage img"), {xPercent: -100, x: 0}, {xPercent: 0}, 0);
 });
-
-function menuSlide() {
-	element = document.getElementById("menuSect");
-	positionInfo = element.getBoundingClientRect();
-	width = positionInfo.width * -1;
-	element.style.right = width + "px";
-	if(count == 0){
-		gsap.to(".menu", {xPercent: -160, duration: 2, opacity: 1}); // Slide out box 1 to right
-		gsap.to(".chatBox", {xPercent: -100, duration: 2, opacity: 1}); // Slide out box 2 to right with a delay
-		count = 1;
-	} else if(count == 1){
-		gsap.to(".menu", {xPercent: -260, duration: 2, delay: 0.5}); // Slide in box 1 from right
-		gsap.to(".chatBox", {xPercent: -360, duration: 3}); // Slide in box 2 from right with a delay
-		count = 0;
-		gsap.to(".menu", {xPercent: 0, duration: 0, delay: 3}); // Slide in box 1 from right
-		gsap.to(".chatBox", {xPercent: 0, duration: 0, delay: 3}); // Slide in box 2 from right with a delay
-	}
-}
-
-document.getElementById("animation").addEventListener("click", menuSlide);
