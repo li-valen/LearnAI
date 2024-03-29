6ef423e4df392c7e571d1049a74a30cb900eec15
Valen Li <117173876+li-valen@users.noreply.github.com>
2024-03-29 14:17:50 -0400

    menu animation better

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -15,9 +15,9 @@ function menuSlide() {
     gsap.to("li", {opacity: 1, duration: 1, ease: "power2.in"});
 		count = 1;
 	} else if(count == 1){
-		gsap.to(".menu", {xPercent: -260, duration: 2, delay: 0.5}); // Slide in box 1 from right
-		gsap.to(".chatBox", {xPercent: -360, duration: 3}); // Slide in box 2 from right with a delay
-    gsap.to("li", {opacity: 0, duration: 1, ease: "power2.inOut"});
+		gsap.to(".menu", {xPercent: -260, duration: 1, delay: 0.35}); // Slide in box 1 from right
+		gsap.to(".chatBox", {xPercent: -360, duration: 2}); // Slide in box 2 from right with a delay
+    gsap.to("li", {opacity: 0, duration: 0.5, ease: "power2.inOut"});
 		count = 0;
 		gsap.to(".menu", {xPercent: 0, duration: 0, delay: 3}); // Slide in box 1 from right
 		gsap.to(".chatBox", {xPercent: 0, duration: 0, delay: 3}); // Slide in box 2 from right with a delay
