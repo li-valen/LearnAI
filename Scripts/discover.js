98316db0cfde1edb246e6843f070ca4cc13b0b00
Rishab <das.rishabk@gmail.com>
2024-04-02 14:24:56 -0400

    commit

--- Scripts/discover.js
+++ Scripts/discover.js
@@ -1,72 +1,21 @@
-// gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
-// var tl = gsap.timeline({delay: 1});
-// const text = gsap.utils.toArray('')
+const text_container = gsap.utils.toArray(".text-container");
 
-
-// //WHAT
-// tl.fromTo("#what-title", {opacity: 0, x: -1000}, 
-//   {scrollTrigger: {trigger: ".what-image",start: "top center",end: "300px 100px",
-//   toggleActions: "play pause restart play",markers: false},
-//   opacity:1,
-//   x:0, 
-//   duration: 1});
-
-// tl.fromTo("#what-p-1", {opacity: 0, x: -1000}, 
-// {scrollTrigger: {trigger: ".what-image",start: "top center",end: "300px 100px",
-// toggleActions: "play pause restart play",markers: false},
-//   opacity:1,
-//   x:0, 
-//   duration: 2});
-  
-
-// tl.fromTo("#what-p-2", {opacity: 0, x: -1000}, 
-// {scrollTrigger: {trigger: ".what-image",start: "top center",end: "300px 100px",
-//   toggleActions: "play pause restart play",markers: false},
-//   opacity:1,
-//   x:0, 
-//   duration: 3});
-
-// //BENEFITS
-// tl.fromTo("#benefits-title", {opacity: 0, x: -1000}, 
-// {scrollTrigger: {trigger: ".benefits-image",start: "top center",end: "300px 100px",
-//   toggleActions: "play pause restart play",markers: true},
-//   opacity:1,
-//   x:0, 
-//   duration: 1});
-
-// tl.fromTo("#benefits-p-1", {opacity: 0, x: -1000}, 
-// {scrollTrigger: {trigger: ".benefits-image",start: "top center",end: "300px 100px",
-//   toggleActions: "play pause restart play",markers: true},
-//   opacity:1,
-//   x:0, 
-//   duration: 2});
-
-// tl.fromTo("#benefits-p-2", {opacity: 0, x: -1000}, 
-// {scrollTrigger: {trigger: ".benefits-image",start: "top center",end: "300px 100px",
-//   toggleActions: "play pause restart play",markers: true},
-//   opacity:1,
-//   x:0, 
-//   duration: 3});
-
-// //DRAWBACKS
-// tl.fromTo("#drawbacks-title", {opacity: 0, x: -1000}, 
-// {scrollTrigger: {trigger: ".drawbacks-image",start: "top center",end: "300px 100px",
-//   toggleActions: "play pause restart play",markers: true},
-//   opacity:1,
-//   x:0, 
-//   duration: 1});
-
-// tl.fromTo("#drawbacks-p-1", {opacity: 0, x: -1000}, 
-// {scrollTrigger: {trigger: ".drawbacks-image",start: "top center",end: "300px 100px",
-//   toggleActions: "play pause restart play",markers: true},
-//   opacity:1,
-//   x:0, 
-//   duration: 2});
-
-// tl.fromTo("#drawbacks-p-2", {opacity: 0, x: -1000}, 
-// {scrollTrigger: {trigger: ".drawbacks-image",start: "top center",end: "300px 100px",
-//   toggleActions: "play pause restart play",markers: true},
-//   opacity:1,
-//   x:0, 
-//   duration: 3});
+text_container.forEach(container=> {
+    gsap.fromTo(container, {
+        x: -1000,
+        opacity: 0,
+    }, 
+    {
+        
+    // scrollTrigger:  {
+    //     trigger: ".text-container",
+    //     start: "top center",
+    //     end: "bottom center",
+    //     t
+    // },
+    x: 0, 
+    opacity: 1,
+    duration: 1})
+});
