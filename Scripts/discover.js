831e183850b3acda73909aff151df8b053300c58
Rishab <das.rishabk@gmail.com>
2024-04-03 13:22:02 -0400

    trying to make fade in animation

--- Scripts/discover.js
+++ Scripts/discover.js
@@ -3,19 +3,32 @@ gsap.registerPlugin(ScrollTrigger);
 const text_container = gsap.utils.toArray(".text-container");
 
 text_container.forEach(container=> {
-    gsap.fromTo(container, {
-        x: -1000,
-        opacity: 0,
-    }, 
-    {
-        
-    // scrollTrigger:  {
-    //     trigger: ".text-container",
-    //     start: "top center",
-    //     end: "bottom center",
-    //     t
-    // },
-    x: 0, 
-    opacity: 1,
-    duration: 1})
+    gsap.fromTo(container, {x: -1000, opacity: 0}, {
+        x: 0, 
+        opacity: 1, 
+        scrollTrigger: {
+            trigger: container,
+            scrub: true,
+            start: "top top",
+            end: "bottom bottom",
+            toggleActions: "play play restart play"
+        }
+
+    })
 });
+
+const images = gsap.utils.toArray("#image");
+
+
+images.forEach(image=> {
+    gsap.fromTo(image, {opacity: 0}, {
+        opacity: 1,
+        scrollTrigger: {
+            trigger: image,
+            start: "top top",
+            end: "bottom bottom",
+            toggleActions: "play play restart play"
+        },
+        duration: 1
+    })
+})
