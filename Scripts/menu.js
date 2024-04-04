86d1824737dcfc6b33645ee0bf5d901544325c72
Valen Li <117173876+li-valen@users.noreply.github.com>
2024-04-03 20:32:01 -0400

    save

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -1,26 +1,20 @@
 let count = 0;
 let element = document.getElementById("menuSect");
 let positionInfo = element.getBoundingClientRect();
-let width = positionInfo.width * -1;
-element.style.right = width + "px";
-
 function menuSlide() {
 	element = document.getElementById("menuSect");
 	positionInfo = element.getBoundingClientRect();
-	width = positionInfo.width * -1;
-	element.style.right = width + "px";
+
 	if(count == 0){
-		gsap.to(".menu", {xPercent: -160, duration: 2, opacity: 1, display:"block"}); // Slide out box 1 to right
-		gsap.to(".chatBox", {xPercent: -100, duration: 2, opacity: 1, display:"block"}); // Slide out box 2 to right with a delay
-    gsap.to("li", {opacity: 1, duration: 1, ease: "power2.in"});
+		gsap.to(".menu", {height: "100%", duration: 0.8, opacity: 1, display:"block", ease: "power3.in"}); // Slide out box 1 to right
+		gsap.to(".chatBox", {height: "100%", duration: 0.8, opacity: 1, display:"block", ease: "power3.in", transformOrigin: "bottom"}); // Slide out box 2 to right with a delay
+		gsap.to("li", {opacity: 1, duration: 1.5, ease: "power2.in"});
 		count = 1;
 	} else if(count == 1){
-		gsap.to(".menu", {xPercent: -260, duration: 1, delay: 0.35}); // Slide in box 1 from right
-		gsap.to(".chatBox", {xPercent: -360, duration: 2}); // Slide in box 2 from right with a delay
-    gsap.to("li", {opacity: 0, duration: 0.5, ease: "power2.inOut"});
+		gsap.to(".menu", {height: "0px", duration: 0.8, ease: "power2.in"}); // Slide in box 1 from right
+		gsap.to(".chatBox", {height: "0px", duration: 0.8, ease: "power2.in"}); // Slide in box 2 from right with a delay
+    	gsap.to("li", {opacity: 0, duration: 0.8, ease: "power2.out"});
 		count = 0;
-		gsap.to(".menu", {xPercent: 0, duration: 0, delay: 3, display:"none"}); // Slide in box 1 from right
-		gsap.to(".chatBox", {xPercent: 0, duration: 0, delay: 3, display:"none"}); // Slide in box 2 from right with a delay
 	}
 }
 
