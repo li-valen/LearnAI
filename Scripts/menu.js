7fe6a05415ca8e895cce0a1b2c97a9ebc0f95aae
Valen Li <117173876+li-valen@users.noreply.github.com>
2024-04-03 20:45:42 -0400

    auto

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -4,12 +4,13 @@ let positionInfo = element.getBoundingClientRect();
 function menuSlide() {
 	element = document.getElementById("menuSect");
 	positionInfo = element.getBoundingClientRect();
-
+	
 	if(count == 0){
 		gsap.to(".menu", {height: "100%", duration: 0.8, opacity: 1, display:"block", ease: "power3.in"}); // Slide out box 1 to right
 		gsap.to(".chatBox", {height: "100%", duration: 0.8, opacity: 1, display:"block", ease: "power3.in", transformOrigin: "bottom"}); // Slide out box 2 to right with a delay
 		gsap.to("li", {opacity: 1, duration: 1.5, ease: "power2.in"});
 		count = 1;
+		document.body.style.overflow = 'hidden';
 	} else if(count == 1){
 		gsap.to(".menu", {height: "0px", duration: 0.8, ease: "power2.in"}); // Slide in box 1 from right
 		gsap.to(".chatBox", {height: "0px", duration: 0.8, ease: "power2.in"}); // Slide in box 2 from right with a delay
@@ -17,6 +18,7 @@ function menuSlide() {
 		count = 0;
 		gsap.to(".menu", {delay: 0.8, display:"none"}); // Slide in box 1 from right
 		gsap.to(".chatBox", {delay: 0.8, display:"none"}); // Slide in box 2 from right with a delay
+		document.body.style.overflowY = 'auto';
 	}
 }
 
