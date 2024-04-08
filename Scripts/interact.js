7ae9f0792c3b46e3fc2fab5e6dfe581cec78a98f
Rishab <das.rishabk@gmail.com>
2024-04-08 17:53:42 -0400

    commit

--- Scripts/interact.js
+++ Scripts/interact.js
@@ -1,3 +1,11 @@
+$("body").mousemove(function (a) {
+  var mouseX = (a.pageX * -1) / 20;
+  var mouseY = (a.pageY * -1) / 25;
+  $("#background-image").css(
+    "background-position",
+    mouseX + "px " + mouseY + "px"
+  );
+});
 
 
 let videoStream;
