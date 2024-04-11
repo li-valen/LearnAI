9ee3432966948431657a6d43cd40fcbc2ef2b976
Rishab <das.rishabk@gmail.com>
2024-04-11 14:16:13 -0400

    commit

--- Scripts/deep_dive.js
+++ Scripts/deep_dive.js
@@ -1,20 +1,20 @@
-gsap.registerPlugin(ScrollTrigger);
-gsap.utils.toArray(".comparisonSection").forEach(section => {
-	let tl = gsap.timeline({
-			scrollTrigger: {
-				trigger: ".comparisonSection",
-				markers: true,
-				start: "top top",
-				end: () => "+=" + section.offsetWidth, 
-				scrub: true,
-				pin: true,
-        anticipatePin: 1
-			},
-			defaults: {ease: "none"}
-		});
-	tl.fromTo(section.querySelector(".afterImage"), {xPercent: 100, x: 0}, {xPercent: 0})
-	  .fromTo(section.querySelector(".afterImage img"), {xPercent: -100, x: 0}, {xPercent: 0}, 0);
-});
+// gsap.registerPlugin(ScrollTrigger);
+// gsap.utils.toArray(".comparisonSection").forEach(section => {
+// 	let tl = gsap.timeline({
+// 			scrollTrigger: {
+// 				trigger: ".comparisonSection",
+// 				markers: true,
+// 				start: "top top",
+// 				end: () => "+=" + section.offsetWidth, 
+// 				scrub: true,
+// 				pin: true,
+//         anticipatePin: 1
+// 			},
+// 			defaults: {ease: "none"}
+// 		});
+// 	tl.fromTo(section.querySelector(".afterImage"), {xPercent: 100, x: 0}, {xPercent: 0})
+// 	  .fromTo(section.querySelector(".afterImage img"), {xPercent: -100, x: 0}, {xPercent: 0}, 0);
+// });
 
 
 $(document).ready(function() {
