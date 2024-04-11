265bd02a0891170e7065c04a1440fec6007b4cdf
Rishab Das <das.rishabk@gmail.com>
2024-04-10 21:57:40 -0400

    commit

--- Scripts/deep_dive.js
+++ Scripts/deep_dive.js
@@ -5,7 +5,6 @@ gsap.utils.toArray(".comparisonSection").forEach(section => {
 				trigger: ".comparisonSection",
 				markers: true,
 				start: "top top",
-        // makes the height of the scrolling (while pinning) match the width, thus the speed remains constant (vertical/horizontal)
 				end: () => "+=" + section.offsetWidth, 
 				scrub: true,
 				pin: true,
@@ -13,9 +12,7 @@ gsap.utils.toArray(".comparisonSection").forEach(section => {
 			},
 			defaults: {ease: "none"}
 		});
-	// animate the container one way...
 	tl.fromTo(section.querySelector(".afterImage"), {xPercent: 100, x: 0}, {xPercent: 0})
-	  // ...and the image the opposite way (at the same time)
 	  .fromTo(section.querySelector(".afterImage img"), {xPercent: -100, x: 0}, {xPercent: 0}, 0);
 });
 
@@ -43,7 +40,6 @@ $(document).ready(function() {
 		start: "top 10%"
 	});
   
-	//creates the fake horizontal scrolling effect
 	const sections = gsap.utils.toArray(".horizontal-scroll-container section");
 	const container = document.querySelector(".horizontal-scroll-container");
 	let horizontalScrollTween = gsap.to(sections, {
